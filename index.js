e2b2d22d1012e153d0448ca590e028d2b38acc24
faxnii009 <farhanali0882788@gmail.com>
2025-01-23 17:15:43 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+function agecalculator() {
+
+    let name = prompt ("Enter your name")
+    let age = document.getElementById ("age1").value ;
+    
+    
+    res = age - 2024
+
+    alert("Dear " + name + " You are " + res + " Year Old")
+
+    document.write("Dear " + name + " You are " + res + " Year Old")
+    
+    console.log("Dear " + name + " You are " + res + " Year Old")   
+
+}
